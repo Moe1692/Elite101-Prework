be565da7c08140f7683a59ec1b95afd9caedf807
Moe1692 <92749786+Moe1692@users.noreply.github.com>
2023-09-05 00:39:08 +0000

    Laid base foundation for chatbot: intro and exit, as well as placeholders for future chats

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('hello world!!!')
+def printList(ops):
+  for i in range(len(ops)):
+    print(f'{i+1}. {ops[i]}')
+
+print('Welcome to GhatCPT.')
+name = input('What\'s you\'re name? ')
+age = input('How old are you, ' + name + '? ')
+print(f'You\'re name is {name} and you are {age} years old.')
+print('How can I help you, ' + name + '?')
+options = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Exit']
+
+
+while True:
+  printList(options)
+  selected = input('Enter an option selection based on number: ')
+  if selected == '5':
+    print('See you later, ' + name + '!')
+    break
